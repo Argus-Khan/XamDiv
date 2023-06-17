8e5d85bebc53720e528cfaa948559c0e376c2ebe
argus.khan <m.a.khan.oea@gmail.com>
2023-06-17 20:15:49 +0200

    Added Submit Api.

--- back-end/src/app.py
+++ back-end/src/app.py
@@ -28,10 +28,12 @@ class Test(BaseModel):
     Code: str
 
 class Exam(BaseModel):
-    professor: str
+    rofessor: str
+    course: str
     date: str
     marks: int
     time: int
+    start_time: int
     note: str
     triesAllowed: int
     questionsNotes: str
@@ -41,6 +43,7 @@ class ExamAns(BaseModel):
     Exam_Id: str
     Std_Id: str
     Sub_time: str
+    Code_Ans: str
 
 
 def match_token():
@@ -51,6 +54,16 @@ def hash_pass(pswd):
     hs.update(pswd.encode())
     return hs.hexdigest()
 
+
+
+@app.get("/api/Proflogin")
+def home(Prof_Id: str , Pswd: str):
+            return {"Response":"GoodLuck!" , "AccessToken" : token}
+            else:
+                return {"Response": "Invaild Student ID"}
+    else:
+        return {"Response": "Invaild Exam ID"}
+
 @app.get("/api/Stdlogin")
 def home(Std_Id: str , Exam_Id: str):
     Exam_Dir = os.path.abspath('..')+"/database/Exams_Data/"+Exam_Id
@@ -60,6 +73,8 @@ def home(Std_Id: str , Exam_Id: str):
             if Std_Id in Std_Data:
                 token = hash_pass(str(datetime.now()))
                 Std_Data[Std_Id]["Access_Token"] = token
+                Std_Data[Std_Id]["Attended"] = True
+                Std_Data[Std_Id]["Status"] = "Logged In"
                 with open(Exam_Dir + "/Students.json", "w") as  xd:
                     json.dump(Std_Data, xd)
                 return {"Response":"GoodLuck!" , "AccessToken" : token}
@@ -71,9 +86,9 @@ def home(Std_Id: str , Exam_Id: str):
 
 @app.get("/api/getExam")
 def exam(Exam_Id: str):
-    Exam_Dir = os.path.abspath('..')+"/database/Exams_Data/"+Exam_Id
-    if(os.path.exists(Exam_Dir)):
-        with open(Exam_Dir+"/Exam.json","r") as xd:
+    Std_Dir = os.path.abspath('..')+"/database/Exams_Data/"+Exam_Id
+    if(os.path.exists(Std_Dir)):
+        with open(Std_Dir+"/Exam.json","r") as xd:
             return json.loads(xd.read())
     else:
         return {"Response": "Invaild Exam ID"}
@@ -90,7 +105,7 @@ def makeExam(Crnt_Exam : Exam):
     os.makedirs(Exam_Dir)
 
     students_list = Crnt_Exam.studentsIds.replace(" ","")
-    students_list = students_list.split(',')
+    students_list = students_list.split('`')
     students_data = {}
     question_list = Crnt_Exam.questionsNotes.split('`')
     question_data = []
@@ -99,7 +114,7 @@ def makeExam(Crnt_Exam : Exam):
 
     for std in students_list:
         os.makedirs(Exam_Dir+"/"+std)
-        students_data[std] = {"attended" : False , "marks": 0 , "Questions_Attemped": 0 , "Status": "Not attemped", "Time_Taken":0, "Access_Token" : "", "Additioanl_time_awarded" : 0}
+        students_data[std] = {"Attended" : False , "Marks": 0 , "Questions_Attemped": 0 , "Status": "Not attemped", "Time_Taken":0, "Access_Token" : "", "Additioanl_time_awarded" : 0}
 
     Crnt_Exam.studentsIds = students_list
     Crnt_Exam.questionsNotes = question_data
@@ -133,6 +148,23 @@ def compileTest(Crnt_Test : Test):
         return {"Response": "Successful Compile", "Cdde_Dir":Code_dir}
 
 @app.post("/api/submitExam")
-def submitExam():
-    pass
+def submitExam(Crnt_Ans: ExamAns):
+    Exam_Dir = os.path.abspath('..')+"/database/Exams_Data/"+Crnt_Ans.Exam_Id
+    with open(Exam_Dir+"/Students.json","r") as xd:
+            Std_Data = json.loads(xd.read())
+    Std_Data[Crnt_Ans.Std_Id]["Time_Taken"] = Crnt_Ans.Sub_time
+    Std_Data[Crnt_Ans.Std_Id]["Status"] = "Submitted Answers"
+    Crnt_Ans.Code_Ans = Crnt_Ans.Code_Ans.split('`')
 
+    for code , i in zip(Crnt_Ans.Code_Ans, range(1,len(Crnt_Ans.Code_Ans)+1)):
+
+        if len(code) > 1 :
+            Std_Data[Crnt_Ans.Std_Id]["Questions_Attemped"] += 1
+
+        Crnt_Src = Exam_Dir+"/"+Crnt_Ans.Std_Id+"/Q"+str(i)+".cpp"
+        with open(Crnt_Src,"w") as fl:
+            fl.write(code)
+
+    with open(Exam_Dir + "/Students.json", "w") as  xd:
+        json.dump(Std_Data, xd)
+    return {"response": "Successfully Submitted"}
