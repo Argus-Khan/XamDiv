0d7d32a9d63f8a1e27896cff41116b6929f0f348
Firebolt <97257564+Firebolt@users.noreply.github.com>
2023-06-18 17:51:27 +0200

    Changed compile get function to not stringify

--- front-end/src/components/CodeEditor.vue
+++ front-end/src/components/CodeEditor.vue
@@ -13,7 +13,7 @@ export default {
       EditorList: [
         '', '', '', '', ''
       ],
-      FullCode: ''
+      Code: ''
     }
   },
   props: ['QuestionNumber', 'SubmitExam', 'Compiling', 'ExamID', 'StdID', 'SubmitQuestion', 'TimeTaken'],
@@ -23,19 +23,19 @@ export default {
         Exam_Id: this.ExamID,
         Std_Id: this.StdID,
         Q_Num: this.QuestionNumber,
-        Code: JSON.stringify(this.EditorList[this.QuestionNumber - 1])
+        Code: this.EditorList[this.QuestionNumber - 1]
       }).then((response) => {
         this.$emit('CompileFinish', response.data.Response)
       })
     },
     SubmitExam: function() {
-      this.FullCode = (JSON.stringify(this.EditorList[0]) + '`' + JSON.stringify(this.EditorList[1]) + '`' + JSON.stringify(this.EditorList[2]) + '`' + JSON.stringify(this.EditorList[3]) + '`' + JSON.stringify(this.EditorList[4])).replaceAll('"', '')
-      console.log(this.FullCode)
+      this.Code = (JSON.stringify(this.EditorList[0]) + '`' + JSON.stringify(this.EditorList[1]) + '`' + JSON.stringify(this.EditorList[2]) + '`' + JSON.stringify(this.EditorList[3]) + '`' + JSON.stringify(this.EditorList[4])).replaceAll('"', '')
+      console.log(this.Code)
       axios.post('http://localhost:8000/api/submitExam', {
         Exam_Id: this.ExamID,
         Std_Id: this.StdID,
         Sub_time: this.TimeTaken,
-        Code_Ans: this.FullCode
+        Code_Ans: this.Code
       })
       .then((response)=>{console.log(response)})
       .catch((error)=>{console.log(error)})
