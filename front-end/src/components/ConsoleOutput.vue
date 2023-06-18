4d5e354d31035094852e27dd22f87dd1b4fde993
RonIsmaili <125833337+RonIsmaili@users.noreply.github.com>
2023-06-18 20:17:33 +0200

    QoL - Console text changes color now

--- front-end/src/components/ConsoleOutput.vue
+++ front-end/src/components/ConsoleOutput.vue
@@ -1,7 +1,9 @@
 <template>
     <v-container fluid class="pa-2 ma-0 temp">
-        {{ Error }}
-    </v-container>
+        <p class="console-text-awaiting" v-if="this.Error === 'Awaiting first compile...'">{{ Error }}</p>
+        <p class="console-text-success" v-if="this.Error === 'Successful Compile'">{{ Error }}</p> 
+        <p class="console-text-error" v-if="this.Error !== 'Successful Compile' && this.Error !== 'Awaiting first compile...'">{{ Error }}</p>
+</v-container>
 </template>
 
 <script>
@@ -11,11 +13,19 @@ export default {
 </script>
 
 <style scoped>
-    .temp {
-        color: #04ff00;
-        overflow-y: auto;
-        max-height: 19.9vh;
-        background-color: #0d0f17;
-        border-top: 5px solid #424242;
-    }
+.temp {
+    overflow-y: auto;
+    max-height: 19.9vh;
+    background-color: #0d0f17;
+    border-top: 5px solid #424242;
+}
+.console-text-awaiting {
+    color: white;
+}
+.console-text-success {
+    color: #04ff00;
+}
+.console-text-error {
+    color: red;
+}
 </style>
