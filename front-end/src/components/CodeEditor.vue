888e2568d0e9557c720f3e717580dd5a3c9f7399
RonIsmaili <125833337+RonIsmaili@users.noreply.github.com>
2023-06-18 20:27:49 +0200

    QoL - Editor has now an outline

--- front-end/src/components/CodeEditor.vue
+++ front-end/src/components/CodeEditor.vue
@@ -1,6 +1,6 @@
 <template>
     <v-container fluid class="pa-5 ma-0 pt-0">
-      <v-textarea v-model="EditorList[QuestionNumber - 1]" class="bg-blue-grey-darken-3 rounded-xl pa-5" color="black" label="Code editor" rows="23" no-resize hide-details>
+      <v-textarea v-model="EditorList[QuestionNumber - 1]" class="bg-blue-grey-darken-3 rounded-xl pa-5" color="black" variant="outlined" label="Code editor" rows="23" no-resize hide-details>
       </v-textarea>
     </v-container>
 </template>
